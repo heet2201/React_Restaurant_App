750f6b320ba0f9b7e4da7ad853a44a53b6434ca0
heet2201 <heetshah221@gmail.com>
2020-10-13 01:01:10 +0530

    Single Page Application Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class Detail extends Component {
-    
-    renderComments(dish) {
-        if(dish != null)
+function RenderDish({dish}) {
+    if(dish != null)
         {
-            const ret = dish.comments.map((comment) => {
+            return(
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+            );
+        }
+        else
+        {
+            return(
+                <div></div>
+            );
+        }
+}
+
+function RenderComment({comments}) {
+    if(comments != null)
+        {
+            const ret = comments.map((comment) => {
                 return(
                         <li key={comment.id}>
                             <div>
@@ -34,46 +54,42 @@ class Detail extends Component {
                 <div></div>
             );
         }
-    }
+}
 
-    renderDish(dish) {
-        if(dish != null)
-        {
-            return(
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-            );
-        }
-        else
-        {
-            return(
-                <div></div>
-            );
-        }
-    }
-
-    render() {
+function Detail(props) {
 
         console.log("DishDetail Componenet render is invoked");
 
         return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/home">Home</Link>
+                        </BreadcrumbItem>
+
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.selectedDish)}
+                            <RenderDish dish={props.dish} />
                         </div>
                         <div className="col-12 col-md-5">
-                            {this.renderComments(this.props.selectedDish)}
+                            <RenderComment comments={props.comments} />
                     </div>
                 </div>
             </div>
         );
-    }
 }
 
 export default Detail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -48,6 +48,14 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return (
+        <Detail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}        
+        />
+      );
+    }
+
     console.log("Main Componenet render is invoked");
     return (
       <div>
@@ -60,6 +68,7 @@ class Main extends Component {
           <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route path="/menu/:dishId" component={DishWithId} />
             <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
           </Switch>
