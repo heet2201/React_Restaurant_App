9306d333d3ccf68b6da76327c25a64cc8be211c7
heet2201 <heetshah221@gmail.com>
2020-10-12 20:31:51 +0530

    Single Page Application Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent'
 import Detail from './DishDetailComponent'
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import './../App.css';
-import {DISHES} from './../shared/dishes'
+import { DISHES } from './../shared/dishes';
+import { COMMENTS } from './../shared/comments';
+import { PROMOTIONS } from './../shared/promotions';
+import { LEADERS } from './../shared/leaders';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -17,6 +22,9 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
       //selectedDish: null
     };
   }
@@ -33,10 +41,13 @@ class Main extends Component {
 
     const HomePage = () => {
       return (
-        <Home />
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
-    
+
     console.log("Main Componenet render is invoked");
     return (
       <div>
@@ -49,6 +60,7 @@ class Main extends Component {
           <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+            <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
           </Switch>
           <Footer />
